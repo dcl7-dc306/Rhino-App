efbd26a3d979dd9e8d8d4afd8d341fc904e44d9d
Roald Ignacio <roaldjap@gmail.com>
2019-06-03 20:49:18 +1200

    Added add product form

---
+++ Rhino-App/add-product.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="add-product.aspx.cs" Inherits="Rhino_App.add_product" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="navigation" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="content" runat="server">
+    <div id="register">
+    <div class="container"> 
+    <div class="row justify-content-center">
+        <div class="jumbotron col-6">
+
+            <h1 class="u-margin-bottom-small">Add Product</h1>
+            
+            <div class="form-group">
+                <asp:Label ID="lblProdName" runat="server" Text="Name:" AssociatedControlID="txtProdName"></asp:Label>
+                <asp:TextBox ID="txtProdName" CssClass="form-control" runat="server"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <asp:Label ID="lblProdDesc" runat="server" Text="Description" AssociatedControlID="txtProdDesc"></asp:Label>
+                <asp:TextBox ID="txtProdDesc" CssClass="form-control" runat="server"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <asp:Label ID="lblProdPrice" runat="server" Text="Price" AssociatedControlID="txtPrice"></asp:Label>
+                <asp:TextBox ID="txtPrice" CssClass="form-control" runat="server" TextMode="Number"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <asp:Label ID="lblImage" runat="server" Text="Upload Image" AssociatedControlID="flProdImage"></asp:Label>
+                <asp:FileUpload ID="flProdImage" runat="server" />
+                <asp:Label ID="lblStatusImage" runat="server" Text=""></asp:Label>
+            </div>
+
+            <div class="form-group">
+                <asp:Button ID="btnAddProduct" CssClass="ml-auto btn btn-block btn-warning" runat="server" Text="Add Product" OnClick="btnAddProduct_Click"  />
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+</asp:Content>
---
+++ Rhino-App/add-product.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+using System.IO;
+
+namespace Rhino_App
+{
+    public partial class add_product : System.Web.UI.Page
+    {
+        private SqlConnection conn;
+        private SqlCommand cmd;
+        String connStr = WebConfigurationManager.ConnectionStrings["Rhino_DB"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            string path = Server.MapPath("images/uploaded-products/");
+
+            if (flProdImage.HasFile)
+            {
+                string ext = Path.GetExtension(flProdImage.FileName);
+                if(ext == ".jpg" || ext == ".png")
+                {
+                    flProdImage.SaveAs(path + flProdImage.FileName);
+                    string imgPath = "images/uploaded-products/" + flProdImage.FileName;
+                    conn = new SqlConnection(connStr);
+                    cmd = new SqlCommand("INSERT INTO tbl_products(name, description, price, image) values (@name, @description, @price, @image)", conn);
+                    cmd.Parameters.AddWithValue("@name", txtProdName.Text);
+                    cmd.Parameters.AddWithValue("@description", txtProdDesc.Text);
+                    cmd.Parameters.AddWithValue("@price", txtPrice.Text);
+                    cmd.Parameters.AddWithValue("@image", imgPath);
+
+                    if( txtProdName.Text == "" || txtProdDesc.Text == "" || txtPrice.Text == "")
+                    {
+                        Response.Write("<script>alert('Failed: All fields must be filled up.')</script>");
+                    }
+                    else
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                        Response.Write("<script>alert('Success: You have successfully added a product')</script>");
+                    }
+
+                }
+                else
+                {
+                    lblStatusImage.Text = "Only .jpg and .png files are allowed";
+                    Response.Write("<script>alert('Failed: Only .jpg and .png files are allowed to upload')</script>");
+                }
+            }
+            else
+            {
+                lblStatusImage.Text = "Please select an Image file";
+            }
+        }
+    }
+}
