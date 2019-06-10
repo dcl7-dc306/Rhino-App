5790b56de1e1931938f1a9117aa04b0fb0d63a75
Marlon Steve <marlonspr@hotmail.com>
2019-06-10 14:07:31 +1200

    Added Product Catalogue on index

--- Rhino-App/index.aspx.designer.cs
+++ Rhino-App/index.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace Rhino_App {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLogout;
+        
+        /// <summary>
+        /// Repeater1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
     }
 }
